6ff6a6ca7e72fe4803203868292fe69e0c32aa04
nimesh-korat <nimeshkorat611@gmail.com>
2024-12-09 12:44:08 +0530

    first commit

--- src/pages/Alchohol.js
+++ src/pages/Alchohol.js
@@ -8,18 +8,16 @@ function Alchoholsensor() {
   const [data, setData] = useState([]);
 
   const getData = async () => {
-    const response = await fetch("http://reactjswithiot.hstn.me/API/fetchalchoholapi.php", {
+    const response = await fetch("https://reactjswithiot.hstn.me/API/fetchalchoholapi.php", {
       method: "GET",
     })
       .then((res) => {
-        console.log(res.json());
         if (!res.ok) {
           throw new Error("Network response was not ok " + res.statusText);
         }
         return res.json();
       })
       .then((res) => {
-        console.log(res); // Log response for debugging
         if (res.data) {
           setData(res.data);
         } else {
@@ -31,7 +29,6 @@ function Alchoholsensor() {
 
   useEffect(() => {
     getData();
-
   }, []);
 
   return (
@@ -62,51 +59,13 @@ function Alchoholsensor() {
                     </tr>
                   </thead>
                   <tbody>
-                    <tr>
-                      <td>1</td>
-                      <td>0</td>
-                      <td>2024-12-05 23:58:25 </td>
-                    </tr>
-                    <tr>
-                      <td>2</td>
-                      <td>0</td>
-                      <td>2024-12-04 22:58:21 </td>
-                    </tr>
-                    <tr>
-                      <td>3</td>
-                      <td>1</td>
-                      <td>2024-12-04 21:18:10 </td>
-                    </tr>
-                    <tr>
-                      <td>4</td>
-                      <td>0</td>
-                      <td>2024-12-03 14:58:21 </td>
-                    </tr>
-                    <tr>
-                      <td>5</td>
-                      <td>0</td>
-                      <td>2024-12-03 10:51:21 </td>
-                    </tr>
-                    <tr>
-                      <td>6</td>
-                      <td>0</td>
-                      <td>2024-12-02 09:28:56 </td>
-                    </tr>
-                    <tr>
-                      <td>7</td>
-                      <td>0</td>
-                      <td>2024-12-02 11:58:45 </td>
-                    </tr>
-                    <tr>
-                      <td>8</td>
-                      <td>0</td>
-                      <td>2024-12-01 04:18:15 </td>
-                    </tr>
-                    <tr>
-                      <td>9</td>
-                      <td>0</td>
-                      <td>2024-12-01 02:28:45 </td>
-                    </tr>
+                    {data.map((item, index) => (
+                      <tr key={index}>
+                        <td>{item.sensor_id}</td>
+                        <td>{item.temp_value}</td>
+                        <td>{item.timestamp}</td>
+                      </tr>
+                    ))}
                   </tbody>
                 </table>
               </div>
@@ -119,10 +78,7 @@ function Alchoholsensor() {
         </div>
         {/* Content End */}
         {/* Back to Top */}
-        <a
-          href="#"
-          className="btn btn-lg btn-primary btn-lg-square back-to-top"
-        >
+        <a href="#" className="btn btn-lg btn-primary btn-lg-square back-to-top">
           <i className="bi bi-arrow-up" />
         </a>
       </div>
